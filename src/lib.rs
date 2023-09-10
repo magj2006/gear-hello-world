f32bd9255c725c2cb1bcfb6976275436ecf13baa
magj2006 <magj2006@gmail.com>
2023-09-10 12:08:31 +0800

    advanced

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
 #![no_std]
-use gstd::{debug, msg, prelude::*};
+use gstd::{debug, msg, prelude::*, ActorId};
+
+static mut GREETING: Option<String> = None;
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum InputMessages {
+    SendHelloTo(ActorId),
+    SendHelloReply,
+}
 
 #[no_mangle]
 extern "C" fn handle() {
-    msg::reply("Hello", 0).expect("Error in sending a reply message");
-    debug!("Program replied \"Hello\" to incoming message")
+    let input_message: InputMessages = msg::load().expect("Error in loading InputMessages");
+    let greeting = unsafe { GREETING.get_or_insert(Default::default()) };
+    match input_message {
+        InputMessages::SendHelloTo(account) => {
+            debug!("Message: SendHelloTo {:?}", account);
+            msg::send(account, greeting, 0).expect("Error in sending Hello message to account");
+        }
+        InputMessages::SendHelloReply => {
+            debug!("Message: SendHelloReply");
+            msg::reply(greeting, 0).expect("Error in sending reply");
+        }
+    }
 }
 
 #[no_mangle]
 extern "C" fn init() {
-    let init_message: String = msg::load().expect("Can't load init message");
-    debug!("Program was initialized with message {:?}", init_message);
+    let greeting: String = msg::load().expect("Can't load init message");
+    debug!("Program was initialized with message {:?}", greeting);
+    unsafe { GREETING = Some(greeting) };
 }
--- tests/hello_world_test.rs
+++ tests/hello_world_test.rs
@@ -1,14 +1,28 @@
 use gstd::String;
 use gtest::{Log, Program, System};
+use hello_world::InputMessages;
 
 #[test]
 fn hello_test() {
     let sys = System::new();
     sys.init_logger();
     let program = Program::current(&sys);
-    let res = program.send(2, String::from("INIT MESSAGE"));
+    let res = program.send(2, String::from("Hello"));
     assert!(!res.main_failed());
-    let res = program.send(2, String::from("HANDLE MESSAGE"));
-    let expected_log = Log::builder().dest(2).payload(String::from("Hello"));
+
+    // test `SendHelloTo`
+    let hello_recipient: u64 = 4;
+    let res = program.send(2, InputMessages::SendHelloTo(hello_recipient.into()));
+    let expected_log = Log::builder()
+        .dest(hello_recipient)
+        .payload(String::from("Hello"));
+    assert!(res.contains(&expected_log));
+
+    // test `SendHelloReply`
+    let hello_recipient: u64 = 2;
+    let res = program.send(2, InputMessages::SendHelloReply);
+    let expected_log = Log::builder()
+        .dest(hello_recipient)
+        .payload(String::from("Hello"));
     assert!(res.contains(&expected_log));
 }
